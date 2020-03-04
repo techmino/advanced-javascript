fe13902f459ec436d6a7270bd06f925ee74a14bf
unknown <techmino99@gmail.com>
2020-03-05 01:21:22 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,44 @@
+// const age = 0;
+
+// if (age){
+//     console.log(" this is true statement");
+// }
+// else{
+//     console.log(" this is false statement");
+// }
+
+// //example -2
+
+// const name = "solaiman";
+
+// if (name){
+//     console.log(" this is true statement");
+// }
+// else{
+//     console.log(" this is false statement");
+// }
+
+//example -3
+
+// const name = "solaiman";
+
+// if (name.length > 0){
+//     console.log(" this is true statement");
+// }
+// else{
+//     console.log(" this is false statement");
+// }
+
+//example -4
+
+const name = "";
+
+if (name){
+    console.log(" this is true statement");
+}
+else{
+    console.log(" this is false statement");
+}
+
+
+
